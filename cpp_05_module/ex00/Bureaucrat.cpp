88b6d2e0de7cc6b8551b8fae4f64b4659f2b319d
Marisol Mago <mmago@si-p4.kzn.21-school.ru>
2022-10-29 21:09:14 +0300

    cpp_05_module

--- cpp_05_module/ex00/Bureaucrat.cpp
+++ cpp_05_module/ex00/Bureaucrat.cpp
@@ -6,7 +6,71 @@
 /*   By: mmago <mmago@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 22:40:48 by mmago             #+#    #+#             */
-/*   Updated: 2022/10/25 22:40:49 by mmago            ###   ########.fr       */
+/*   Updated: 2022/10/29 16:59:49 by mmago            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(){
+
+}
+
+Bureaucrat::Bureaucrat(const std::string newName, int newGrade) : name( newName ){
+	if (newGrade < 1)
+		throw GradeTooHighException();
+	if (newGrade > 150)
+		throw GradeTooLowException();
+	else {
+		this->grade = newGrade;
+		std::cout << BOLDGREEN"Bureaucrat " << BOLDYELLOW << this->name << BOLDGREEN" named constructor called"RESET << std::endl;
+	}
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat &equals){
+	std::cout << BOLDGREEN"Bureaucrat Assignation operator called"RESET << std::endl;
+	if (this == &equals)
+		return (*this);
+	return (*this);
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy){
+	std::cout << BOLDGREEN"Bureaucrat Copy constructor called"RESET << std::endl;
+	*this = copy;
+}
+
+void				Bureaucrat::setIncremet(){
+	if (this->grade == 1)
+		throw GradeTooHighException();
+	this->grade--;
+};
+
+void				Bureaucrat::setDecrement(){
+	if (this->grade == 150)
+		throw GradeTooLowException();
+	this->grade++;
+};
+
+std::string	Bureaucrat::getName() const{
+	return (name);
+};
+int			Bureaucrat::getGrade() const{
+	return (grade);
+};
+
+const char* Bureaucrat::GradeTooLowException::what() const throw(){
+	return (BOLDBLACK"Error: grade > 150."RESET);
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw(){
+	return (BOLDBLACK"Error: grade < 1."RESET);
+}
+
+Bureaucrat::~Bureaucrat(){
+	std::cout << BOLDRED"Bureaucrat "BOLDYELLOW << this->name << BOLDRED" destructor called"RESET << std::endl;
+}
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &bur){
+	out << BOLDYELLOW << bur.getName() << BOLDCYAN" Bureaucrat grade "BOLDYELLOW << bur.getGrade() << RESET << std::endl;
+	return (out);
+};
--- cpp_05_module/ex00/main.cpp
+++ cpp_05_module/ex00/main.cpp
@@ -6,7 +6,40 @@
 /*   By: mmago <mmago@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 22:40:55 by mmago             #+#    #+#             */
-/*   Updated: 2022/10/25 22:40:56 by mmago            ###   ########.fr       */
+/*   Updated: 2022/10/29 17:03:01 by mmago            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	try
+	{
+		Bureaucrat Mark("Mark", 140);
+		// Bureaucrat Mark("Mark", 1403456);
+		Mark.setDecrement();
+		std::cout << Mark;
+	}
+	catch (std::exception &M)
+	{
+		std::cerr << M.what() << std::endl;
+	}
+
+	Bureaucrat Elton("Elton", 2);
+	std::cout << Elton;
+
+	Elton.setIncremet();
+	std::cout << Elton;
+
+	try
+	{
+		Elton.setIncremet();
+	}
+	catch (std::exception &E)
+	{
+		std::cerr << E.what() << std::endl;
+	}
+	std::cout << Elton;
+	return (0);
+}
